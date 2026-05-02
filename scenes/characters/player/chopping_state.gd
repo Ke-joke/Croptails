ba244ea644afbe99defccd23e38b5f422d55e0c6
NEO <1204198410@qq.com>
2026-05-03 01:02:03 +0800

    创建player行为状态

---
+++ scenes/characters/player/chopping_state.gd
@@ -0,0 +1,24 @@
+extends NodeState
+
+@export var player: Player
+@export var animated_sprite_2D: AnimatableBody2D
+
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	pass
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
